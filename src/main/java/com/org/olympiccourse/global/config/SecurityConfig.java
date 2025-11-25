304dcbd03b71a89f88d3d4aee4865af5b37b293d
zzuharchive <welcome-ing@naver.com>
2025-11-25 23:35:34 +0900

    chore: 로그인 기능 구현에 필요한 config 파일 작성

---
+++ src/main/java/com/org/olympiccourse/global/config/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.org.olympiccourse.global.config;
+
+import com.org.olympiccourse.global.security.CustomAuthenticationProvider;
+import com.org.olympiccourse.global.security.basic.CustomUserDetailsService;
+import com.org.olympiccourse.global.security.exception.CustomAuthenticationEntryPoint;
+import com.org.olympiccourse.global.security.filter.JwtAuthorizationFilter;
+import com.org.olympiccourse.global.security.jwt.JwtUtil;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider() {
+        return new CustomAuthenticationProvider(customUserDetailsService, passwordEncoder());
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(JwtUtil jwtUtil) {
+        return new JwtAuthorizationFilter(jwtUtil);
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        return new ProviderManager(List.of(customAuthenticationProvider()));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(auth -> auth
+                // 로그인, 회원가입, 중복체크
+                .requestMatchers("/api/auth/login", "/api/users", "/api/users/check"
+                ).permitAll()
+                .anyRequest().authenticated()
+            );
+
+        // 기본 로그인 관련 설정
+        http
+            .formLogin(auth -> auth.disable())
+            .httpBasic(auth -> auth.disable())
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // csrf 비활성화
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http
+            .addFilterAfter(jwtAuthorizationFilter(jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
+
+        http.exceptionHandling(
+            exception -> exception.authenticationEntryPoint(customAuthenticationEntryPoint));
+
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource));
+
+        return http.build();
+    }
+}

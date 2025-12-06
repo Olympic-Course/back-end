552616d6ab5d58893b6bad23d9cadf7b1412c4e9
zzuharchive <welcome-ing@naver.com>
2025-12-06 23:41:26 +0900

    fix: s3config 파일 수정

--- src/main/java/com/org/olympiccourse/global/config/S3Config.java
+++ src/main/java/com/org/olympiccourse/global/config/S3Config.java
@@ -4,7 +4,7 @@ package com.org.olympiccourse.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -21,7 +21,7 @@ public class S3Config {
     public S3Presigner S3Presigner() {
         return S3Presigner.builder()
             .region(Region.of(region))
-            .credentialsProvider(ProfileCredentialsProvider.create(profile))
+            .credentialsProvider(DefaultCredentialsProvider.builder().build())
             .build();
     }
 }
